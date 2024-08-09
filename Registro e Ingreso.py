3df3fddb0f87286a52acc48d097e2ae8ea41b81d
Antonio <aleman.antonio.95@gmail.com>
2024-08-09 16:19:18 -0600

    Crear codigo para ingreso de usuarios ya registrados

--- Registro e Ingreso.py
+++ Registro e Ingreso.py
@@ -1,5 +1,7 @@
 #Impotación de librerias
 import sqlite3
+import sys
+import time
 
 #Crear Conexión a la base de de datos SQLite3
 conn = sqlite3.connect("Cuentas.db")
@@ -52,6 +54,7 @@ while True:
         
 #Bucle True para pedir al usuario que ingrese sus datos
 while True: 
+    #Bucle en caso de uqe usuario desee registrarse
     if accion == registro:
         nombre_1 = str(input("\nIngresa tu primer nombre\n"))
         nombre_2 = str(input("\nIngresa tu segundo nombre\n"))
@@ -81,12 +84,38 @@ while True:
         cursor_login.execute("SELECT * FROM usuarios WHERE correo=?",(correo,))
         result_login = cursor_login.fetchone()
         if result_login:
-            print("\nEl correo " +correo+ " ya esta registrado\n")  
+            print("\nEl correo " +correo+ " ya esta registrado\n")
+            time.sleep(5)
+            sys.exit()
         else:
             cursor_login.execute("INSERT INTO usuarios (Correo,Nombre1,Nombre2,Apellido_Paterno,Apellido_Materno,Celular,Contraseña)VALUES(?,?,?,?,?,?,?)",
             (correo,nombre_1,nombre_2,apellido_1,apellido_2,telefono,contraseña_2))
-            print("\nRegistro con exito")
+            print("\nRegistro con exito\n")
+            print(f"Bienvenido ' {nombre_1}''{apellido_1}'")
             conn_login.commit()
             conn_login.close()
             break
+        #Bucle en caso de que el usuario desee ingresar
+    if accion == ingreso:
+        #Ingreso de correo y contraseña
+        correo = str(input("Ingresa su correo: \n"))
+        contraseña_1 = str(input("\nIngrese su contraseña: \n"))
+            
+        #Crea la Variable Query para buscar los datos ingresados
+        conn_correo =sqlite3.connect("Registros.db")
+        cursor_correo = conn_correo.cursor()
+        query = """SELECT *FROM usuarios WHERE correo = ? AND contraseña = ?"""
+            
+        #Ejecutar el query para relaizar la busqueda
+        cursor_correo.execute(query, (correo, contraseña_1))
+        result_correo = cursor_correo.fetchone()
+            
+        #Validación si se cuentra en la base de datos, contar las columas de la tabla para que jale la info
+        if result_correo:
+            print("Bienvenido " +result_correo[2]+" "+result_correo[3])
+            break
+        else:
+            print(f"No existe ningun correo registrado como: '{correo}'\no ingreso erroneamente la contraseña ")
+            
+            
 
